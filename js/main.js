3913b3ed1bd6e3c68754a04e93b02844e16b3e8d
noah112k <50031627+noah112k@users.noreply.github.com>
2019-08-16 17:37:34 -0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+  document.querySelector("body").classList.remove("fade-in");
+};
+
+//Select DOM Items
+//Menu button, menu overlay, branding, menu items
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+//Set initial state of menu
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+    showMenu = false;
+  }
+}
